939dc98bb04994577a72d3d491b2a04547ba5fbd
raymonde <raymonde@sfu.ca>
2018-03-09 00:40:03 -0800

    Actually add server files

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+function onRequest(request, response) {
+	response.writeHead(200, {"Context-Type": "text/plain"});
+	response.write("Hello Brogrammers");
+	response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+console.log("Server is running");
