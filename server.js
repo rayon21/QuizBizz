5dec54f92a6f6cf5bf3d3552b93b24de684b50e2
ParmJohal <parmj@sfu.ca>
2018-03-15 15:59:11 -0700

    added to server.js so that it serves static files from the react app

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require('path');
 const _ = require('lodash');
 var http = require("http");
+
 
 var app = express();
 const port = process.env.PORT || 8888;
@@ -17,9 +19,32 @@ function onRequest(request, response) {
 // })
 
 //endpoint test
+
+//*****  API CALLS *****
+
 app.get('/api/:version', function(req, res) {
     res.send(req.params.version);
   });
+
+app.get('/api/quizzes/:teacherID', function(req, res) {
+
+	//connect to mongoDB, get quizzes based on teacher ID
+    res.send(req.params);
+
+  });
+
+//***** REACT FILES *****
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
