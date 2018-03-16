2e4e71e8bf6d3d3a9dc4445f604870fd4ef18ab6
raymonde <raymonde@sfu.ca>
2018-03-16 02:24:32 -0700

    /users endpoint to store users

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const path = require('path');
 const _ = require('lodash');
+const bodyParser = require('body-parser');
 var http = require("http");
+var {mongoose} = require('./db/mongoose.js');
 
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 8888;
+
+app.use(bodyParser.json());
 
 function onRequest(request, response) {
 	response.writeHead(200, {"Context-Type": "text/plain"});
@@ -13,10 +18,18 @@ function onRequest(request, response) {
 	response.end();
 }
 
-// app.post('/users', (req, res) => {
-// 	var body = _.pick(req.body, ['email', 'password']);
-// 	var user = new User(body);
-// })
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	})
+});
 
 //endpoint test
 
