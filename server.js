f9a1ce4c45aea6d245118c7ddde68c2967369caa
ParmJohal <parmj@sfu.ca>
2018-03-19 14:25:59 -0700

    add sockets to package.json

--- server.js
+++ server.js
@@ -153,6 +153,12 @@ app.patch('/quizzes/:id', authenticate, (req, res) => {
   })
 });
 
+// SOCKET
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+io.on('connection', function(){ console.log(HELLO); });
+
 
 //*****  API CALLS *****
 
