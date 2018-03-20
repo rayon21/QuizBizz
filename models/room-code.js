b24c5130032cde892f8cea271bbdb3918d76084c
I Ting Chen <itingc@sfu.ca>
2018-03-20 10:17:48 -0700

    create room code

---
+++ models/room-code.js
@@ -0,0 +1,8 @@
+const _ = require('lodash');
+/*
+code = last in ary
+keep counting up if hit 'ZZZZ', then restart
+check if the new code == to first of ary if yes, +1
+*/
+var code = "AAAA";
+var codeArr = [];
