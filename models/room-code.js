f51b2e86a83c740c97ae13e3272e9c3635b0e42b
I Ting Chen <itingc@sfu.ca>
2018-03-20 18:25:13 -0700

    modified room-codejs to generate random number

--- models/room-code.js
+++ models/room-code.js
@@ -2,42 +2,31 @@ String.prototype.replaceAt=function(index, replacement) {
     return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
 }
 
-/*keep counting up if hit 'ZZZZ', then restart*/
-var incrementCode = (code) => {
-    var i = 3;
-    while(i >= 0){
-        if(code[i] === 'Z'){
-            code = code.replaceAt(i,'a');
-            i--;
-        }else if(code[i] === 'z'){
-            code = code.replaceAt(i,'A');
-            break;
-        }else{
-            var codeNum = code.charCodeAt(i);
-            code = code.replaceAt(i, String.fromCharCode(codeNum + 1));
-            break;
-        }
+
+var randomCode = () => {
+    
+    var code = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 4; i++){ 
+        code += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return code;
 };
 
-var generateCode = (arrLen) => {
-    var code = "aaaa";
+var generateCode = (codeArr) => {
+    var code;
     var isDuplicate ;
-
-    if(arrLen !== 0){
-        code = codeArr[arrLen-1];
-        console.log(code);
-        do{
-            code = incrementCode(code);
-            isDuplicate = codeArr.includes(code); //check if code alrdy exist
-        }while (isDuplicate === true);
-    }
+    
+    do{
+        code = randomCode();
+        isDuplicate = codeArr.includes(code); //check if code alrdy exist
+    }while (isDuplicate === true);
+    
     return code;
 };
 
 var addCode = (codeArr) =>{
-    var code = generateCode(codeArr.length);
+    var code = generateCode(codeArr);
     codeArr.push(code);
 };
 
@@ -45,14 +34,6 @@ var deleteCode = (codeArr, codeTodelete) =>{
     var idx = codeArr.indexOf(codeTodelete);
     codeArr.splice(idx, 1);
 };
-
-
-/*demostrate
-var codeArr = ["aaaa", "ZZZZ"];
-addCode(codeArr);
-console.log(codeArr);
-deleteCode(codeArr, "aaaa");
-console.log(codeArr);*/
 
 module.exports = {
     addCode,
