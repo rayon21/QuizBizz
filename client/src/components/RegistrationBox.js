b694619025b35598b6216982d4f9f3aefc39991e
raymonde <raymonde@sfu.ca>
2018-03-23 09:59:51 -0700

    Add material-ui to registration page

--- client/src/components/RegistrationBox.js
+++ client/src/components/RegistrationBox.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import TextField from 'material-ui/TextField';
+
+
+var validator = require('validator');
 
 class RegistrationBox extends Component {
 	constructor() {
 		super();
 		this.state = {
 			email: '',
-			password: ''
+			password: '',
+			validEmail: null,
+			validPassword: null
 		}
 		this.handleEmail = this.handleEmail.bind(this);
 		this.handlePassword = this.handlePassword.bind(this);
 		this.register = this.register.bind(this);
+		this.validPassword = this.validatePassword.bind(this);
 	}
 
 	handleEmail(e) {
@@ -20,6 +27,12 @@ class RegistrationBox extends Component {
 
 	handlePassword(e) {
 		this.setState({password: e.target.value});
+	}
+
+	handleChange = name => event => {
+		this.setState({
+			[name]: event.target.value
+		})
 	}
 
 	register(e) {
@@ -33,25 +46,51 @@ class RegistrationBox extends Component {
 		}).catch((e) => console.log(e));
 	}
 
-	componentDidMount() {
-		
+	validatePassword = (e) => {
+		if (e.target.value.length < 6) {
+			console.log('password too short');
+			this.setState({validPassword: false});
+		} else {
+			this.setState({validPassword: true});
+		}
+	}
+
+	validateEmail = (e) => {
+		this.setState({
+			validEmail: validator.isEmail(e.target.value)
+		});
 	}
 
 	render() {
+		var x = false;
 		return (
 			<div className="d-flex pr-5 pl-5 flex-column">
-				<div className="col-sm-12 pb-3 pt-4 mb-3 reg-container">
-					<h4 className="mt-4 mb-4">Join with your email</h4>
+				<div className="col-sm-12 pb-3 pt-4 mb-4 reg-container">
+					<h4 className="mt-4 mb-2">Join with your email</h4>
 					<form action="" className="">
-						<div className="form-group">
-							<input type="email" className="form-control" id="email-input" aria-describedby="emailHelp" placeholder="email" onChange={this.handleEmail}/>
-						</div>
-						<div className="form-group">
-							<input type="password" className="form-control" id="password-input" aria-describedby="passwordHelp" placeholder="password" onChange={this.handlePassword}/>
-						</div>
-						<div className="row d-flex justify-content-end mr-2 ml-2 mt-4">
-							<button type="submit" className="btn btn-primary" onClick={this.register}>register</button>
-						</div>
+						<TextField
+					      fullWidth
+					      type="email"
+					      label="Email"
+					      margin="normal"
+					      onChange={this.handleChange('email')}
+					      onBlur={this.validateEmail}
+					      error={this.state.validEmail || this.state.validEmail == null ? false : true}
+					      helperText={this.state.validEmail || this.state.validEmail == null ? "" : "Enter a valid email"}
+					    />
+						<TextField
+					      fullWidth
+					      label="Password"
+					      type="password"
+					      margin="normal"
+					      onChange={this.handleChange('password')}
+					      onBlur={this.validatePassword}
+					      error={this.state.validPassword || this.state.validPassword == null ? false : true}
+					      helperText={this.state.validPassword || this.state.validPassword == null ? "" : "Password must be 6 characters in length"}
+					    />
+					    <div className="text-right">
+					    	<button className="btn btn-primary mt-4 mb-3" onClick={this.register}>register</button>
+					    </div>
 					</form>
 				</div>
 				<div className="row">
