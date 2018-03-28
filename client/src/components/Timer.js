6d47b5270fd8de858a092062c82fb171ad12ee89
I Ting Chen <itingc@sfu.ca>
2018-03-27 23:25:25 -0700

    add sound effect to timer

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -5,38 +5,44 @@ class Timer extends Component {
     constructor() {
         super();
         this.state = { 
-            seconds: 60 
+            seconds: 2 
         };
-        this.timer = 0;
+        this.timer = -1;
         this.startTimer = this.startTimer.bind(this);
         this.countDown = this.countDown.bind(this);
-      }
-    
-      startTimer() {
-        if (this.timer === 0) {
-          this.timer = setInterval(this.countDown, 1000);
+        this.complete = this.complete.bind(this);
+    }
+
+    startTimer() {
+        if (this.timer === -1) {
+            this.timer = setInterval(this.countDown, 1000);
         }
-      }
-    
-      countDown() {
+    }
+
+    countDown() {
         let seconds = this.state.seconds - 1;
         this.setState({
-          seconds: seconds,
+            seconds: seconds,
         });
         
-        if (seconds === 0) { 
-          clearInterval(this.timer);
-        }
-      }
+        if (this.state.seconds === 0) {this.complete()}
+    }
+
+    complete() {
+        clearInterval(this.timer);
+        let audio = document.getElementById("audio");
+        audio.play(); 
+    }
     
-      render() {
+    render() {
         return(
             <div>
                 {this.state.seconds} seconds left 
                 <button onClick={this.startTimer}>Start</button>
+                <audio id="audio" ><source src="http://www.orangefreesounds.com/wp-content/uploads/2014/08/Wrong-answer-sound-effect.mp3?_=1" type="audio/mpeg" /></audio>
             </div>
         );
-      }
+    }
 
 }
 
