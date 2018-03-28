4534933f015082764869645c058c865f6e96d09d
I Ting Chen <itingc@sfu.ca>
2018-03-28 12:06:14 -0700

    resolve merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,6 @@
 import React, { Component }             from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD
-import HeaderGuest from './components/HeaderGuest.js';
-import RegistrationBox from './components/RegistrationBox.js';
-import LoginBox from './components/LoginBox.js';
-import QuizzesPage from './components/QuizzesPage';
-import QuizPage from './components/QuizPage';
-import CreateQuizPage from './components/CreateQuizPage';
-import PresenterPage from './components/PresenterPage';
-import Timer from './components/Timer';
-=======
+
 import Header                           from './components/Header.js';
 import RegistrationPage                 from './components/RegistrationPage.js';
 import LoginPage                        from './components/LoginPage.js';
@@ -20,9 +11,7 @@ import SocketClient                     from './components/SocketClient';
 import PresenterPage                    from './components/PresenterPage';
 import PlayerRoom                       from './components/PlayerRoom';
 import ParticipantsBox                  from './components/ParticipantsBox';
-
->>>>>>> 6d114ffe7fca058fe86fd83e1949a2114ee76fac
-
+import Timer                            from './components/Timer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -38,12 +27,9 @@ class App extends Component {
           <Route path="/create" component={CreateQuizPage}/>
           <Route path="/join" component={SocketClient}/>
           <Route path="/room/:id" component={PresenterPage}/>
-<<<<<<< HEAD
           <Route path="/timer" component={Timer}/>
-=======
           <Route path="/play/:id" component={PlayerRoom}/>
           <Route path="/box" component={ParticipantsBox}/>
->>>>>>> 6d114ffe7fca058fe86fd83e1949a2114ee76fac
         </Switch>
       </BrowserRouter>
     );
